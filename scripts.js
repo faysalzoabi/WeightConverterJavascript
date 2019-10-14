54f4199624ef8293a59bc4d1093b48124f416929
faysal Al Zoabi <faysal.zoabi@gmail.com>
2019-10-14 22:51:31 +0200

    first commit

---
+++ scripts.js
@@ -0,0 +1,13 @@
+const inputValue = document.getElementById('lbsInput');
+
+document.getElementById('output').style.visibility = 'hidden';
+inputValue.addEventListener('input', updateValue)
+
+function updateValue(e) {
+    document.getElementById('output').style.visibility = 'visible';
+    let lbs = e.target.value;
+    document.getElementById('gramsOutput').innerHTML = lbs/0.0022046;
+    document.getElementById('KgOutput').innerHTML = lbs/2.2046;
+    document.getElementById('ozOutput').innerHTML = lbs*16;
+
+}
